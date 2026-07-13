30c75932e605ad3579b68e33ff77346b29671c17
Vishal7hub <vishal9748pra@gmail.com>
2026-07-13 19:53:40 +0530

    feat: setup FastAPI application and project configuration

---
+++ app/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+
+from app.core.config import settings
+from app.core.logger import logger
+
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION,
+    description="Production-ready facial recognition attendance system.",
+)
+
+
+@app.on_event("startup")
+async def startup():
+    logger.info("Application Started Successfully")
+
+
+@app.get("/")
+async def root():
+    return {
+        "message": settings.APP_NAME,
+        "version": settings.APP_VERSION,
+    }
+
+
+@app.get("/health")
+async def health():
+    return {
+        "status": "healthy"
+        
+    }
